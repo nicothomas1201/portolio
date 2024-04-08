7a1ec2225958f56c20f0573e830c28fdfb90e9fe
Nicolas Quintero <quinterorodrigueznicolas@gmail.com>
2024-04-08 09:48:03 -0500

    change tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
